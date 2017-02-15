a1dbd2b492a350c2ab6972323ac61d067a982170
Blake Cerecero <Blake@DigitalBlake.com>
2017-02-14 21:51:52 -0600

    [zsh] Updated theme-unit-test function
    Signed-off-by: Blake Cerecero <Blake@DigitalBlake.com>

--- general-cli/custom-scripts/theme-unit-test.zsh
+++ general-cli/custom-scripts/theme-unit-test.zsh
@@ -1,8 +1,13 @@
 #!/bin/zsh
 
-theme-unit-test () {
+SERVER_DIR='/var/www/public_html'
+theme-unit-test() {
+	WORKING_DIR=$(pwd);
+
+	cd $SERVER_DIR
 	curl https://wpcom-themes.svn.automattic.com/demo/theme-unit-test-data.xml >> theme-unit-test-data.xml
 	wp plugin install wordpress-importer --activate --allow-root
 	wp import theme-unit-test-data.xml --authors=create --allow-root
 	rm theme-unit-test-data.xml
+	cd $WORKING_DIR
 }
