869dd66b4eb4c62ca3f1e30f9127f4741c66c789
Blake Cerecero <Blake@DigitalBlake.com>
2017-07-10 02:04:33 -0500

    [conf, zsh, Dockerfile, vim] Added vim and tmux configes

--- general-cli/custom-scripts/setup-scripts.zsh
+++ general-cli/custom-scripts/setup-scripts.zsh
@@ -7,6 +7,25 @@ alias bower="bower --allow-root"
 alias rm="rm -i"
 alias npmg="npm list -g --depth=0"
 
+# aliases for Tmux
+alias t='tmux -2'
+alias ta='tmux attach -t'
+alias td='tmux detach'
+alias tnew='tmux new -s'
+alias tls='tmux ls'
+alias tkill='tmux kill-session -t'
+
+# convenience aliases for editing configs
+alias ev='vim ~/.config/nvim/init.vim'
+alias et='vim ~/.tmux.conf'
+alias ez='vim ~/.zshrc'
+
+# make sure that if a program wants you to edit
+# text, that Vim is going to be there for you
+export EDITOR="vim"
+export USE_EDITOR=$EDITOR
+export VISUAL=$EDITOR
+
 # NVM Load Script
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
